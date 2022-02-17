a7e0d2b08a98e9f528366e3a514c013ea9db637f
Fco Javier <francisco.franco.cuadrado.al@iespoligonosur.org>
2022-02-17 12:35:19 +0100

    Cambios en la clase de IAW

---
+++ round.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+	<?php 
+		$x = 8 - 6.4;
+		$y = 1.6;
+
+		echo $x;
+		echo "<br>";
+		echo $y . "<br>";
+
+		var_dump($x);
+		echo "<br>";
+		var_dump($y);
+
+
+		echo "<br>";
+
+		if ($x == $y) {
+			echo "Iguales";
+		} else {
+			echo "Distintos";
+		}
+		echo "<br>";
+		var_dump($x == $y);
+
+
+	
+		echo "<br>";
+		$x = round ($x,2);
+		$y = round ($y,2);
+	
+		var_dump($x == $y);
+
+	?>
+
+
+
+
+</body>
+</html>
